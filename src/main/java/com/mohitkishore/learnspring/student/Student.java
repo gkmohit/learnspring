17fa27d143527953fdff32fbe7f6808699688991
Mohit Kishore <gkmohit@Mohits-MacBook-Air.local>
2022-06-03 12:30:41 -0400

    Initial commit

---
+++ src/main/java/com/mohitkishore/learnspring/student/Student.java
@@ -0,0 +1,81 @@
+package com.mohitkishore.learnspring.student;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private int age;
+    private LocalDate dob;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(long id, String name, int age, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, int age, LocalDate dob, String email) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
